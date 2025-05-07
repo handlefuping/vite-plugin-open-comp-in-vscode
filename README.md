7de70dcc9c068365e8fe22289338695fcea5302d
handlefuping <fansh01@mingyuanyun.com>
2025-05-07 21:43:57 +0800

    .

---
+++ README.md
@@ -0,0 +1,6 @@
+## 开发环境下
+
+你可以按住command键，点击元素打开vscode
+
+
+![图片alt](./image.png "图片title")
--- index.js
+++ index.js
@@ -1,56 +1,111 @@
 import HTML from 'html-parse-stringify'
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
+import { join } from 'path';
 
+const PLUGINNAME = 'open-comp-in-vscode'
+const VSCODELINK = 'vscode://file://'
+const KEY = 'vite-plugin-meta-key-down'
+let scriptContent = `
+  /* ${PLUGINNAME}插件注入 */
+  document.addEventListener("click", (e) => {
+    let current = e.target
+    let vscodeLink = current.getAttribute("data-file");
+    while (!vscodeLink && current.parentElement && current.parentElement.nodeName != 'BODY') {
+      current = current.parentElement
+      vscodeLink = current.getAttribute("data-file");
+    }
+    if (vscodeLink && vscodeLink.startsWith("${VSCODELINK}") && document.body.classList.contains('${KEY}')) {
+      let linkElement = document.createElement("a");
+      linkElement.setAttribute("href", vscodeLink);
+      linkElement.click();
+      linkElement.remove();
+      linkElement = null;
+      /* 三方唤起keyup事件中断 */
+      document.body.classList.remove('${KEY}')
+    }
+  });
+  document.addEventListener('keydown', (e) => {
+    if (e.key == "Meta") {
+      document.body.classList.add('${KEY}')
+    }
+  });
+  document.addEventListener('keyup', (e) => {
+    if (e.key == "Meta") {
+      document.body.classList.remove('${KEY}')
+    }
+  });
+`
+const styleContent = `
+  /* ${PLUGINNAME}插件注入 */
+  .${KEY} [data-file^="${VSCODELINK}"]:hover {
+    color: #4078f2
+  }
+`
+const templateReg = /<(template)>([\s\S]*)<\/\1>/
 
-
+const stepupRenderReg = /(setup\s*\([\s\S]*\s+(?:return|\([^)]*\)\s*=>)\s*\(?\s*)(<([^>]+)>[\s\S]*<\/\3>)(\s*\)?\s*})/
 
 const openCompInVscode = ({
   ext = ['.vue', '.jsx', '.tsx'],
-  entry = 'main.js'
 } = {}) => {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = dirname(__filename);
   const loadFile = []
+  let isDev = false
+  let envDir = ''
   return {
     enforce: 'pre',
-    name: 'open-comp-in-vscode',
+    name: PLUGINNAME,
+    configResolved(resolvedConfig) {
+      isDev = resolvedConfig.mode === 'development'
+      envDir = resolvedConfig.envDir
+    },
     load(id) {
-      if (id.startsWith(join(__dirname, 'src')) && ext.some(item => id.endsWith(item))) {
+      if (isDev && id.startsWith(join(envDir, 'src')) && ext.some(item => id.endsWith(item))) {
         loadFile.push(id)
       }
     },
-    transform(code, id) {
-      if (id.startsWith(join(__dirname, 'src')) && id.endsWith(entry)) {
-        return code + `
-
-        document.addEventListener("click", (e) => {
-          let vscodeLink = e.target.getAttribute("data-file")
-          if (vscodeLink && vscodeLink.startsWith("vscode://") && e.metaKey) {
-            let linkElement = document.createElement("a");
-            linkElement.setAttribute("href", vscodeLink);
-            linkElement.click();
-            linkElement.remove();
-            linkElement = null;
-          }
-        });
-        `
+    transformIndexHtml(html) {
+      if (isDev) {
+        
+        return {
+          html,
+          tags: [{
+            tag: 'script',
+            children: scriptContent,
+            injectTo: 'body'
+          }, {
+            tag: 'style',
+            children: styleContent,
+            injectTo: 'body'
+          }]
+        }
       }
+      
+    },
+    transform(code, id) {
       if (loadFile.includes(id)) {
-        let newCode = code.replace(/<template>([\s\S]*)<\/template>/, (match, html) => {
-          if (!html) {
-            return match
-          }
+        const transformHtml = (html) => {
           var ast = HTML.parse(html)
-
           ast.forEach((node) => {
             const { type, name, attrs } = node
             if (type === 'tag' && /^[a-z]+$/.test(name)) {
-              attrs['data-file'] = `vscode://file://${id}`
-
+              attrs['data-file'] = `${VSCODELINK}${id}`
             }
           })
-          return `<template>${HTML.stringify(ast)}</template>`
+          return HTML.stringify(ast)
+        }
+
+        let newCode = code.replace(templateReg, (match, group1, html) => {
+          if (!html) {
+            return match
+          }
+          
+          return `<template>${transformHtml(html)}</template>`
+        })
+        newCode = newCode.replace(stepupRenderReg, (match, group1, html, group3, group4) => {
+          if (!html) {
+            return match
+          }
+          return `${group1}${transformHtml(html)}${group4}`
+
         })
         return newCode
 
